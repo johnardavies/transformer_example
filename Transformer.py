842eead56245c2558f4351c1b0d2a3e1b4555cbb
johnardavies <johnardavies@gmail.com>
2024-06-09 12:02:40 +0100

    edits to Transformer class

--- Transformer.py
+++ Transformer.py
@@ -1,18 +1,81 @@
 import torch
 import torch.nn.functional as Fun
 from torch import nn
-from config import *
+from config import TransformerConfig
+
+# Initialize configuration
+config = TransformerConfig()
+
+
+class LayerNorm(nn.Module):
+    """ LayerNorm but with an optional bias. PyTorch doesn't support simply bias=False """
+
+    def __init__(self, ndim, bias):
+        super().__init__()
+        self.weight = nn.Parameter(torch.ones(ndim))
+        self.bias = nn.Parameter(torch.zeros(ndim)) if bias else None
+
+    def forward(self, input):
+        return Fun.layer_norm(input, self.weight.shape, self.weight, self.bias, 1e-5)
+
+
+class Embedding(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        # Creates the text embedding and the position embedding
+        # nn.Embedding automatically does the one hot encoding so this
+        # does not need to be created directly
+        self.config = config
+        self.wte = nn.Embedding(config.vocab_size, config.dim_embedding)
+        self.wtp = nn.Embedding(config.block_size, config.dim_embedding)
+
+    def forward(self, x):
+        # Generates the word embedding from the text
+        x = self.wte(x)
+        #  Generates the position embedding is applied over a tensor representing word position 
+        position_ids = (
+            torch.arange(self.config.block_size).unsqueeze(0).repeat(x.size(0), 1)
+        )
+        position_embeddings = self.wtp(position_ids)
+        # Add the two embeddings
+        x = x + position_embeddings
+
+        return x
+
+
+class Processing_layer(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.denselayer1 = nn.Linear(config.dim_embedding, config.dim_inner_layer)
+        self.denselayer2 = nn.Linear(config.dim_inner_layer, config.dim_embedding)
+        self.layernorm1 = LayerNorm(config.dim_embedding, bias=config.bias)
+        self.layernorm2 = LayerNorm(config.dim_embedding, bias=config.bias)
+
+    def forward(self, x):
+        # A layer norm and then two dense layers with a skip connection and then layer norm again
+        x_in = self.layernorm1(x)
+        x = self.denselayer1(x_in)
+        x = self.denselayer2(x) + x_in
+        x = self.layernorm2(x)
+        return x
 
 
 class MultiHeadAttention(nn.Module):
     def __init__(self, config):
         super().__init__()
+        # Checks that the dimesion of the embedding vector can be divided by the number of heads
         assert config.dim_embedding % config.n_head == 0
-        # key, query, value projections for all heads, but in a batc
-        # nn.Linear applies a linear y=Ax +b transformation to the input
+
+        # set embedding and head sizes
+        self.n_head = config.n_head
+        self.dim_embedding = config.dim_embedding
+
+        # nn.Linear applies a linear y=Ax+b transformation to the input
         # the input dimension is the first argument
         # the output dimension is the second argument
         # the last argument is the b (bias) term
+
+        # Sets up a layer that increases the value of the embedding 3x to calculate the query, key and value vectors
         self.c_attn = nn.Linear(
             config.dim_embedding, 3 * config.dim_embedding, bias=config.bias
         )
@@ -20,17 +83,25 @@ class MultiHeadAttention(nn.Module):
         self.c_proj = nn.Linear(
             config.dim_embedding, config.dim_embedding, bias=config.bias
         )
+
         # regularization
         self.attn_dropout = nn.Dropout(config.dropout)
         self.resid_dropout = nn.Dropout(config.dropout)
-        self.n_head = config.n_head
-        self.dim_embedding = config.dim_embedding
         self.dropout = config.dropout
-        # flash attention make GPU go brrrrr but support is only in PyTorch >= 2.0
+
+        # Uses a faster implementation of attention if scaled_dot_product_attention available in module torch.nn.functional
+        # (which it is from PyTorch >= 2.0)
         self.flash = hasattr(torch.nn.functional, "scaled_dot_product_attention")
         if not self.flash:
             print(
                 "WARNING: using slow attention. Flash Attention requires PyTorch >= 2.0"
+            )
+            # causal mask to ensure that attention is only applied to the left in the input sequence
+            self.register_buffer(
+                "bias",
+                torch.tril(torch.ones(config.block_size, config.block_size)).view(
+                    1, 1, config.block_size, config.block_size
+                ),
             )
 
     def forward(self, x):
@@ -71,6 +142,8 @@ class MultiHeadAttention(nn.Module):
             # manual implementation of attention
             # Calculate the inner product of the q and k vectors and normalise by square root of length of key vectors
             att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+            # Calculate the masked attention
+            #   att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
             # Apply the softmax layer so that everything sums to 1
             att = F.softmax(att, dim=-1)
             att = self.attn_dropout(att)
@@ -80,74 +153,23 @@ class MultiHeadAttention(nn.Module):
         y = (
             y.transpose(1, 2).contiguous().view(B, T, C)
         )  # re-assemble all head outputs side by side
+        #   print(y.size(), 'proj')
         # output projection
         y = self.resid_dropout(self.c_proj(y))
-
+        # print(y.size())
         return y
 
 
-class Embedding(nn.Module):
+class CausalSelfAttentionMasked(nn.Module):
     def __init__(self, config):
         super().__init__()
-        #  Creates the text embedding and the position embedding
-        # The embedding automatically does the one hot encoding so this
-        # does not need to be created directly
-        self.config = config
-        self.wte = nn.Embedding(config.vocab_size, config.dim_embedding)
-        self.wtp = nn.Embedding(config.block_size, config.dim_embedding)
-
-    def forward(self, x):
-        # Applies the word embedding and then adds it to the position embedding
-        x = self.wte(x)
-        # The position embedding is applied over a tensor that ranges 0 to 20
-        # The embedding is torch.Size([20, 200]) and is broadcast onto the larger tensor
-        position_ids = (
-            torch.arange(self.config.block_size).unsqueeze(0).repeat(x.size(0), 1)
-        )
-        position_embeddings = self.wtp(position_ids)
-        x = x + position_embeddings
-
-        return x
-
-
-class LayerNorm(nn.Module):
-    """LayerNorm but with an optional bias. PyTorch doesn't support simply bias=False"""
-
-    def __init__(self, ndim, bias):
-        super().__init__()
-        self.weight = nn.Parameter(torch.ones(ndim))
-        self.bias = nn.Parameter(torch.zeros(ndim)) if bias else None
-
-    def forward(self, input):
-        return Fun.layer_norm(input, self.weight.shape, self.weight, self.bias, 1e-5)
-
-
-class ProcessingLayer(nn.Module):
-    def __init__(self, config):
-        super().__init__()
-        self.denselayer1 = nn.Linear(config.dim_embedding, config.dim_inner_layer)
-        self.denselayer2 = nn.Linear(config.dim_inner_layer, config.dim_embedding)
-        self.layernorm1 = LayerNorm(config.dim_embedding, bias=config.bias)
-        self.layernorm2 = LayerNorm(config.dim_embedding, bias=config.bias)
-
-    def forward(self, x):
-        # A layer norm and then two dense layers\n",
-        x_in = self.layernorm1(x)
-        x = self.denselayer1(x_in)
-        x = self.denselayer2(x) + x_in
-        x = self.layernorm2(x)
-        return x
-
-
-class MaskedMultiHeadAttention(nn.Module):
-    def __init__(self, config):
-        super().__init__()
+        # Checks that the dimesion of the embedding vector can be divided by the number of heads
         assert config.dim_embedding % config.n_head == 0
-        # key, query, value projections for all heads, but in a batc
-        # nn.Linear applies a linear y=Ax +b transformation to the input
-        # the input dimension is the first argument
-        # the output dimension is the second argument
-        # the last argument is the b (bias) term
+        
+        # set embedding and head size
+        self.n_head = config.n_head
+        self.dim_embedding = config.dim_embedding
+
         self.c_attn = nn.Linear(
             config.dim_embedding, 3 * config.dim_embedding, bias=config.bias
         )
@@ -155,13 +177,14 @@ class MaskedMultiHeadAttention(nn.Module):
         self.c_proj = nn.Linear(
             config.dim_embedding, config.dim_embedding, bias=config.bias
         )
+
         # regularization
         self.attn_dropout = nn.Dropout(config.dropout)
         self.resid_dropout = nn.Dropout(config.dropout)
-        self.n_head = config.n_head
-        self.dim_embedding = config.dim_embedding
         self.dropout = config.dropout
-        # flash attention make GPU go brrrrr but support is only in PyTorch >= 2.0
+
+        # Uses a faster implementation of attention if scaled_dot_product_attention available in module torch.nn.functional
+        # (which it is from PyTorch >= 2.0)
         self.flash = hasattr(torch.nn.functional, "scaled_dot_product_attention")
         if not self.flash:
             print(
@@ -176,7 +199,7 @@ class MaskedMultiHeadAttention(nn.Module):
             )
 
     def forward(self, x):
-        # Split the input tensor dimension down into the batch size B , the sequence length T and the embedding dimensionality C
+        # Split the input tensor dimension down into the batch size B , the sequence length T and the emnbedding dimensionality C
         (
             B,
             T,
@@ -207,6 +230,7 @@ class MaskedMultiHeadAttention(nn.Module):
                 dropout_p=self.dropout if self.training else 0,
                 is_causal=True,
             )
+        #    print(y.size())
         else:
             # manual implementation of attention
             # Calculate the inner product of the q and k vectors and normalise by square root of length of key vectors
@@ -234,7 +258,11 @@ class EncoderDecoderAttention(nn.Module):
     def __init__(self, config):
         super().__init__()
         assert config.dim_embedding % config.n_head == 0
-        # key, query, value projections for all heads, but in a batch
+
+       # set embedding and head sizes
+        self.n_head = config.n_head
+        self.dim_embedding = config.dim_embedding
+ 
         self.c_attn_en = nn.Linear(
             config.dim_embedding, 3 * config.dim_embedding, bias=config.bias
         )
@@ -246,14 +274,14 @@ class EncoderDecoderAttention(nn.Module):
         self.c_proj = nn.Linear(
             config.dim_embedding, config.dim_embedding, bias=config.bias
         )
+
         # regularization
         self.attn_dropout = nn.Dropout(config.dropout)
         self.resid_dropout = nn.Dropout(config.dropout)
-        self.n_head = config.n_head
-        self.dim_embedding = config.dim_embedding
-
         self.dropout = config.dropout
-        # flash attention make GPU go brrrrr but support is only in PyTorch >= 2.0
+
+        # Uses a faster implementation of attention if scaled_dot_product_attention available in module torch.nn.functional
+        # (which it is from PyTorch >= 2.0)
         self.flash = hasattr(torch.nn.functional, "scaled_dot_product_attention")
         if not self.flash:
             print(
@@ -284,6 +312,7 @@ class EncoderDecoderAttention(nn.Module):
             1, 2
         )  # (B, nh, T, hs)
         v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+        #   print(q.size(), k.size())
 
         # Generating the value vector from  the decoder input
         (
@@ -314,6 +343,7 @@ class EncoderDecoderAttention(nn.Module):
         else:
             # manual implementation of attention
             att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+            att = att.masked_fill(attn_mask == 0, float("-inf"))
             att = F.softmax(att, dim=-1)
             att = self.attn_dropout(att)
             y = att @ v  # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
@@ -327,22 +357,19 @@ class EncoderDecoderAttention(nn.Module):
         return y
 
 
-# The encoder class
-
-
 class Encoder(nn.Module):
     def __init__(self, config):
         super().__init__()
         self.config = config
 
-        # Create embeddings for the encoder
+        # Create embeddings for both the encoder and the decoder
         self.encoder_embed = Embedding(config)
 
         # Create an attention mechanism for the encoder
         self.attention_encoder = MultiHeadAttention(config)
 
         # Set up a processing layer
-        self.encoder_processing_layer = ProcessingLayer(config)
+        self.encoder_processing_layer = Processing_layer(config)
 
     def forward(self, x):
         # Encode the language we want to translate from
@@ -357,19 +384,16 @@ class Encoder(nn.Module):
         return x
 
 
-# The decoder class
-
-
 class Decoder(nn.Module):
     def __init__(self, config):
         super().__init__()
         self.config = config
 
-        # Create an embedding layer for the decoder
+        # Create embeddings for both the encoder and the decoder
         self.decoder_embed = Embedding(config)
 
         # Create an attention mechanism for the decoder
-        self.attention_decoder = MaskedMultiHeadAttention(config)
+        self.attention_decoder = CausalSelfAttentionMasked(config)
 
         # Create a layernorm layer
         self.layernorm = LayerNorm(config.dim_embedding, bias=config.bias)
@@ -377,10 +401,10 @@ class Decoder(nn.Module):
         # Create the encoder decoder attention
         self.decoder_attn = EncoderDecoderAttention(config)
 
-        # Set up a processing layer for the decoder
-        self.decoder_processing_layer = ProcessingLayer(config)
+        # Set up a processing sections one for the encoder and the other for the decoder
+        self.decoder_processing_layer = Processing_layer(config)
 
-        # The final layer which maps the models embedding dimension back to the vocab size
+        # The final layer which maps the models embedding dimension batch to the vocab size
         self.final_layer = nn.Linear(config.dim_embedding, config.vocab_size)
 
     def forward(self, x, y):
@@ -391,22 +415,17 @@ class Decoder(nn.Module):
         # Apply the attention mechanism and add the input
         y = self.attention_decoder(y) + y
 
-        # Apply layer norm
         y = self.layernorm(y)
 
-        # Take the output from the encoder and last layer of decoder and calculate attention again then add the input
+        # Take the ouput from the encoder x and last layer of decoder y and calculate attention again then add the input
         y = self.decoder_attn(y, x) + y
 
         # apply layer norm, two dense layers and a layer norm again
         y = self.decoder_processing_layer(y)
 
-        # Map the embedding dimension back to the vocabularly size
         y = self.final_layer(y)
 
         return y
-
-
-# The Transformers class
 
 
 class Transformer(nn.Module):
@@ -416,7 +435,7 @@ class Transformer(nn.Module):
         assert config.block_size is not None
         self.config = config
 
-        # Create both the encoder and the decoder layers
+        # Create embeddings for both the encoder and the decoder
         self.encoder = Encoder(config)
         self.decoder = Decoder(config)
 
@@ -424,7 +443,9 @@ class Transformer(nn.Module):
         # Encode the language we want to translate from
         encoder_out = self.encoder(x)
 
-        # Take the output from the encoder and translated text and pass to the decoder
+        # Apply the attention mechanism and add the input
         y = self.decoder(encoder_out, y)
 
         return y
+
+
