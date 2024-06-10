4fb16070e6f14b37aa13bdf18fe902c4ba88e102
johnardavies <johnardavies@gmail.com>
2024-06-10 08:29:49 +0100

    Update config.py reorder config

--- config.py
+++ config.py
@@ -5,13 +5,13 @@ from dataclasses import dataclass
 class TransformerConfig:
     """With a dataclass we don't need to write an init function, just specify class attributes and their types"""
 
-    block_size: int = 20
-    batch_size: int = 30
-    epochs: int = 2
-    vocab_size: int = 15000
+    vocab_size: int = 15000  
     dim_embedding: int = 600
-    dim_inner_layer: int = 2000
+    dim_inner_layer: int =  2000
     n_head: int = 6
+    batch_size: int = 30 
+    block_size: int = 20 
+    epochs: int = 30
     dropout: float = 0.2
     bias: bool = False
 
